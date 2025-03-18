72e877de03d8f067a3d8accb64b3a248488baf80
manngo2013 <manngo2013@gmail.com>
2025-03-18 17:14:48 +0700

    Code ReactJS Lecture 11 - Introduction to JSX

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
-
+const studentName = "Adam Nguyen";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h1>Hello: {studentName}</h1>
+    </div>
+  );
 }
 
 export default App;
