ab4fc34cfcc0713be29d11dda8e88479a9effd46
manngo2013 <manngo2013@gmail.com>
2025-03-20 08:02:02 +0700

    Code ReactJS Lecture 12 - Rendering Elements in ReactJS

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
 const studentName = "Adam Nguyen";
+const age = 18;
+
+const employee = {
+  username: "John",
+  age: 20,
+};
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello: {studentName}</h1>
+      <h1>
+        Hello: {studentName} - {age}
+      </h1>
+      <br />
+      <h2>
+        Employee: {employee.username} - {employee.age}
+      </h2>
     </div>
   );
 }
